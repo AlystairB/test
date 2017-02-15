ba1a3eb13e2aab3c934581aaa57ad574aa10f6c1
monkey <monkey@BB21014>
2017-02-15 16:55:30 -0600

    lel

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnPatriarchy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnPatriarchy
@@ -40,21 +41,34 @@
             this.btnPatriarchy.Text = "REEEEEEEEEEE";
             this.btnPatriarchy.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Patriarchy";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnPatriarchy);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnPatriarchy;
+        private System.Windows.Forms.Label label1;
     }
 }
 
