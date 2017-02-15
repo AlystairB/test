43de199b86dd60f068366cdf5e20fe16bbeb8e1f
monkey <monkey@BB21013>
2017-02-15 16:49:16 -0600

    I added a button

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPatriarchy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPatriarchy
+            // 
+            this.btnPatriarchy.Location = new System.Drawing.Point(79, 114);
+            this.btnPatriarchy.Name = "btnPatriarchy";
+            this.btnPatriarchy.Size = new System.Drawing.Size(124, 67);
+            this.btnPatriarchy.TabIndex = 0;
+            this.btnPatriarchy.Text = "REEEEEEEEEEE";
+            this.btnPatriarchy.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnPatriarchy);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPatriarchy;
     }
 }
 
